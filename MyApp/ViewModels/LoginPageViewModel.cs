93cd4274e0fb7473586d53fbdaced1e8fd6868bb
Prakash M <prakashvjr2@gmail.com>
2023-10-30 10:59:20 +0530

    Add project files.

---
+++ MyApp/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,44 @@
+﻿
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using MyApp.Models;
+using MyApp.Pages;
+using MyApp.Services;
+using Newtonsoft.Json;
+
+namespace MyApp.ViewModels
+{
+    public partial class LoginPageViewModel : BaseViewModel
+    {
+        [ObservableProperty]
+        private string _userName;
+
+        [ObservableProperty]
+        private string _password;
+        readonly ILoginRepository loginRepository = new LoginService();
+
+        [ICommand]
+        public async void Login()
+        { 
+            if(!string.IsNullOrWhiteSpace(UserName) && !string.IsNullOrWhiteSpace(Password))
+            {
+                UserInfo userInfo = await loginRepository.Login(UserName, Password);
+                if(userInfo != null)
+                {
+                    string userDetails = JsonConvert.SerializeObject(userInfo);
+                    Preferences.Set(nameof(App.UserInfo), userDetails);
+
+                    App.UserInfo = userInfo;
+
+                    await Shell.Current.GoToAsync($"//{nameof(HomePage)}");
+                }
+                else
+                {
+                    await App.Current.MainPage.DisplayAlert("Alert", "Invalid login, try again!!!", cancel: "OK");
+                }
+                
+            }
+        }
+        
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,4 @@
+## Project Name = My_Login_App Using API
+## This is My First MAUI App
+## Login => home Page => Emplyoee page => Add Emplyoee => Employee List
+ 
